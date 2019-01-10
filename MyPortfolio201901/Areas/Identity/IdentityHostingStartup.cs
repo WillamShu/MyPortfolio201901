2e8bdb94474a355f3a50b035620ba4ee8d65c4a4
Will Shu <wshu@eyekor.com>
2019-01-10 11:04:15 -0600

    add scaffolding files

---
+++ MyPortfolio201901/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyPortfolio201901.Data;
+
+[assembly: HostingStartup(typeof(MyPortfolio201901.Areas.Identity.IdentityHostingStartup))]
+namespace MyPortfolio201901.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
