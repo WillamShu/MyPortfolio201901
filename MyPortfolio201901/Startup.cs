d9059984dbe18c38ac00dceab5a9395a226d8b66
WilliamShu <fshu2@wisc.edu>
2019-01-11 23:05:45 -0600

    add qr codes and send email function

--- MyPortfolio201901/Startup.cs
+++ MyPortfolio201901/Startup.cs
@@ -7,11 +7,13 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyPortfolio201901.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MyPortfolio201901.Services;
 
 namespace MyPortfolio201901
 {
@@ -41,6 +43,9 @@ namespace MyPortfolio201901
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
